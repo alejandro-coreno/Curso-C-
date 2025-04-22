697e4af1cb1958bffa419dd0c58aba99979ca257
josue <alejandro@MacBook-Air-de-josue.local>
2025-04-21 21:27:36 -0600

    Modificacion de proyecto

--- SentenciaIF/obj/Debug/net9.0/SentenciaIF.AssemblyInfo.cs
+++ SentenciaIF/obj/Debug/net9.0/SentenciaIF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SentenciaIF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89374e97b29a2a6507245f1b81b10d7a937c202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95b954424bd2dc695579474a51f5ae9c1620437")]
 [assembly: System.Reflection.AssemblyProductAttribute("SentenciaIF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SentenciaIF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
